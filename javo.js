f735342429e4c31d6f492053675649be046007f4
Flytach <m42pedrosa@gmail.com>
2022-07-12 01:53:42 +0200

    index.html

---
+++ javo.js
@@ -0,0 +1,19 @@
+let message;
+
+message= 'Holo';
+
+alert(message);
+
+let user = 'Otto', age = 33, msg = 'holo';
+
+let $ = 1;
+let _ = 2;
+
+const mahBirthday = 'cumple dia';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+
+let color = COLOR_RED;
+
+alert(color);
